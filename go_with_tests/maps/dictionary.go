a34a699133f1d521b83534159a3eda9bc1228e79
Wayne Manselle <wayne@viridianforge.tech>
2022-09-21 23:05:19 -0700

    Continued map lesson

--- go_with_tests/maps/dictionary.go
+++ go_with_tests/maps/dictionary.go
@@ -1,7 +1,20 @@
 package maps
 
+import (
+	"errors"
+)
+
+var ErrNotFound = errors.New("that search term could not be found")
+
 type Dictionary map[string]string
 
-func (d Dictionary) Search(search string) string {
-	return d[search]
+func (d Dictionary) Add(term string, definition string) {
+	d[term] = definition
+}
+
+func (d Dictionary) Search(term string) (string, error) {
+	if d[term] == "" {
+		return "", ErrNotFound
+	}
+	return d[term], nil
 }
--- go_with_tests/maps/dictionary_test.go
+++ go_with_tests/maps/dictionary_test.go
@@ -7,10 +7,29 @@ import (
 func TestSearch(t *testing.T) {
 	dictionary := Dictionary{"test": "this is only a test"}
 
-	got := dictionary.Search("test")
-	expected := "this is only a test"
+	t.Run("Valid Search", func(t *testing.T) {
+		got, _ := dictionary.Search("test")
+		expected := "this is only a test"
 
-	assertStrings(t, got, expected)
+		assertStrings(t, got, expected)
+	})
+
+	t.Run("Invalid Search", func(t *testing.T) {
+		_, got := dictionary.Search("not_a_term")
+		expected := ErrNotFound
+
+		assertError(t, got, expected)
+	})
+}
+
+func TestAdd(t *testing.T) {
+	dictionary := Dictionary{}
+
+	t.Run("Add to an empty dictionary", func(t *testing.T) {
+		dictionary.Add("new", "a new addition")
+		expected := "a new addition"
+		assertDefinition(t, dictionary, "new", expected)
+	})
 }
 
 func assertStrings(t testing.TB, got string, expected string) {
@@ -19,4 +38,22 @@ func assertStrings(t testing.TB, got string, expected string) {
 	if got != expected {
 		t.Errorf("got %q, expected %q", got, expected)
 	}
+}
+
+func assertError(t testing.TB, got error, expected error) {
+	t.Helper()
+
+	if got != expected {
+		t.Errorf("got %q, expected %q", got, expected)
+	}
+}
+
+func assertDefinition(t testing.TB, dict Dictionary, term string, definition string) {
+	t.Helper()
+	got, err := dict.Search("new")
+	if err != nil {
+		t.Fatal("should find the added term", err)
+	}
+
+	assertStrings(t, got, definition)
 }
