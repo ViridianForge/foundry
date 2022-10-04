842009308be06b9fc3f014333f48c966ec53cefb
Wayne Manselle <wayne@viridianforge.tech>
2022-10-03 22:00:33 -0700

    WIP

---
+++ go_with_tests/mocks/mocks.go
@@ -0,0 +1,35 @@
+package mocks
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"time"
+)
+
+const finalWord = "Go!"
+const countDownStart = 3
+
+type Sleeper interface {
+	Sleep()
+}
+
+type SpySleeper struct {
+	Calls int
+}
+
+func (s *SpySleeper) Sleep() {
+	s.Calls++
+}
+
+func Countdown(out io.Writer) {
+	for i := countDownStart; i > 0; i-- {
+		fmt.Fprintln(out, i)
+		time.Sleep(1 * time.Second)
+	}
+	fmt.Fprint(out, finalWord)
+}
+
+func main() {
+	Countdown(os.Stdout)
+}
