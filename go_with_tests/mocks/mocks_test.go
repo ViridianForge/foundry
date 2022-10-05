84d24f56151566fbda3e768f706358c6f7fe5321
Wayne Manselle <wayne@viridianforge.tech>
2022-10-04 23:01:21 -0700

    Complete mocks lesson

--- go_with_tests/mocks/mocks_test.go
+++ go_with_tests/mocks/mocks_test.go
@@ -2,21 +2,59 @@ package mocks
 
 import (
 	"bytes"
+	"reflect"
 	"testing"
+	"time"
 )
 
 func TestCountdown(t *testing.T) {
-	buffer := &bytes.Buffer{}
 
-	Countdown(buffer)
+	t.Run("prints 3 to Go!", func(t *testing.T) {
+		buffer := &bytes.Buffer{}
+		Countdown(buffer, &SpyCountdownOperations{})
 
-	got := buffer.String()
-	expected := `3
+		got := buffer.String()
+		expected := `3
 2
 1
 Go!`
 
-	if got != expected {
-		t.Errorf("got %q expected %q", got, expected)
+		if got != expected {
+			t.Errorf("expected %q, observed %q", expected, got)
+		}
+
+	})
+
+	t.Run("validate a sleep before each print", func(t *testing.T) {
+		spySleepPrinter := &SpyCountdownOperations{}
+
+		Countdown(spySleepPrinter, spySleepPrinter)
+
+		expected := []string{
+			write,
+			sleep,
+			write,
+			sleep,
+			write,
+			sleep,
+			write,
+		}
+
+		if !reflect.DeepEqual(expected, spySleepPrinter.Calls) {
+			t.Errorf("expected calls %v, observed %v", expected, spySleepPrinter.Calls)
+		}
+	})
+}
+
+func TestConfigurableSleeper(t *testing.T) {
+	sleepTime := 5 * time.Second
+
+	spyTime := &SpyTime{}
+	sleeper := ConfigurableSleeper{sleepTime, spyTime.Sleep}
+	sleeper.Sleep()
+
+	if spyTime.durationSlept != sleepTime {
+		t.Errorf("Expected duration slept of %d, observed %d", sleepTime, spyTime.durationSlept)
 	}
+
 }
