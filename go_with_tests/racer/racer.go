cf4b537077626239395ab5aa03f4c6363558ef18
Wayne Manselle <wayne@viridianforge.tech>
2022-10-30 22:03:21 -0700

    Finish select lesson

--- go_with_tests/racer/racer.go
+++ go_with_tests/racer/racer.go
@@ -1,20 +1,33 @@
 package racer
 
 import (
+	"fmt"
 	"net/http"
 	"time"
 )
 
-func Racer(url1 string, url2 string) string {
-	if measureResponseTime(url1) < measureResponseTime(url2) {
-		return url1
-	}
+var tenSecondTimeout = 10 * time.Second
 
-	return url2
+func Racer(url1 string, url2 string) (string, error) {
+	return ConfigurableRacer(url1, url2, tenSecondTimeout)
 }
 
-func measureResponseTime(url string) time.Duration {
-	refTime := time.Now()
-	http.Get(url)
-	return time.Since(refTime)
+func ConfigurableRacer(url1 string, url2 string, timeout time.Duration) (string, error) {
+	select {
+	case <-ping(url1):
+		return url1, nil
+	case <-ping(url2):
+		return url2, nil
+	case <-time.After(timeout):
+		return "", fmt.Errorf("timed out waiting for %s and %s", url1, url2)
+	}
+}
+
+func ping(url string) chan struct{} {
+	ch := make(chan struct{})
+	go func() {
+		http.Get(url)
+		close(ch)
+	}()
+	return ch
 }
